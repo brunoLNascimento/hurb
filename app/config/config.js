0f6b10f2b2790f7e2b54c34dd37313f3aec09ba1
blnascimento <bruno.liman@sonda.com>
2024-02-14 14:37:25 -0300

    ajuste no config

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,10 @@
 const config = {
     db: {
-        url: 'mongodb+srv://bruno:bruno@cluster0.h6epz.mongodb.net/?retryWrites=true&w=majority',
-        urlDocker: 'mongodb://mongo:27017/desafioHurb',
-        urlTeste: 'mongodb://localhost:27017/desafioHurbTest',
+        // url: 'mongodb+srv://bruno:bruno@cluster0.h6epz.mongodb.net/?retryWrites=true&w=majority',
+        // urlDocker: 'mongodb://mongo:27017/desafioHurb',
+        url: 'mongodb://localhost:27017/desafioHurbTest',
+        
+        urlDocker: 'mongodb://localhost:27017/desafioHurbTest',
         options: {
             server: {
                 socketOptions: {keepAlive: 1}
